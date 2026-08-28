e92b021c4398218f66c73045e5a3d17e51bbcd0c
Isra Amin <150552777+IsraAmin@users.noreply.github.com>
2026-08-28 21:45:42 +0300

    Show admin activity log

--- src/pages/AdminWorkspacePage.tsx
+++ src/pages/AdminWorkspacePage.tsx
@@ -1,9 +1,11 @@
 import { AdminPage } from './AdminPage'
 import { AdminUserDirectoryPanel } from '../components/AdminUserDirectoryPanel'
+import { AdminActivityLog } from '../components/AdminActivityLog'
 
 export function AdminWorkspacePage() {
   return <div className="admin-workspace-stack">
     <AdminPage />
+    <AdminActivityLog />
     <AdminUserDirectoryPanel />
   </div>
 }
