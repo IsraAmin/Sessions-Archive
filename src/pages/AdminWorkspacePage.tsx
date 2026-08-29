bf43fe802766715cc77136cf5f369344a07d7f52
Isra Amin <150552777+IsraAmin@users.noreply.github.com>
2026-08-29 21:34:57 +0300

    Restore original admin workspace start

--- src/pages/AdminWorkspacePage.tsx
+++ src/pages/AdminWorkspacePage.tsx
@@ -1,14 +1,12 @@
 import { AdminPage } from './AdminPage'
 import { AdminUserDirectoryPanel } from '../components/AdminUserDirectoryPanel'
 import { AdminActivityLog } from '../components/AdminActivityLog'
-import { SessionSpeakersManager } from '../components/SessionSpeakersManager'
 import { useAuth } from '../hooks/useAuth'
 
 export function AdminWorkspacePage() {
   const { isSuperAdmin } = useAuth()
 
   return <div className="admin-workspace-stack">
-    <SessionSpeakersManager />
     <AdminPage />
     {isSuperAdmin && <AdminActivityLog />}
     <AdminUserDirectoryPanel />
