617a603f9b8322b02b7de0366435c9d5f3df1c35
Isra Amin <150552777+IsraAmin@users.noreply.github.com>
2026-08-29 21:27:45 +0300

    Surface multi-speaker manager in admin workspace

--- src/pages/AdminWorkspacePage.tsx
+++ src/pages/AdminWorkspacePage.tsx
@@ -1,12 +1,14 @@
 import { AdminPage } from './AdminPage'
 import { AdminUserDirectoryPanel } from '../components/AdminUserDirectoryPanel'
 import { AdminActivityLog } from '../components/AdminActivityLog'
+import { SessionSpeakersManager } from '../components/SessionSpeakersManager'
 import { useAuth } from '../hooks/useAuth'
 
 export function AdminWorkspacePage() {
   const { isSuperAdmin } = useAuth()
 
   return <div className="admin-workspace-stack">
+    <SessionSpeakersManager />
     <AdminPage />
     {isSuperAdmin && <AdminActivityLog />}
     <AdminUserDirectoryPanel />
