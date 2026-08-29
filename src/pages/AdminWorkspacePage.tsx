4322209287b3473e47b480ad47ab79400602422f
Isra Amin <150552777+IsraAmin@users.noreply.github.com>
2026-08-29 16:37:21 +0300

    Restrict admin activity log to super admin

--- src/pages/AdminWorkspacePage.tsx
+++ src/pages/AdminWorkspacePage.tsx
@@ -1,11 +1,14 @@
 import { AdminPage } from './AdminPage'
 import { AdminUserDirectoryPanel } from '../components/AdminUserDirectoryPanel'
 import { AdminActivityLog } from '../components/AdminActivityLog'
+import { useAuth } from '../hooks/useAuth'
 
 export function AdminWorkspacePage() {
+  const { isSuperAdmin } = useAuth()
+
   return <div className="admin-workspace-stack">
     <AdminPage />
-    <AdminActivityLog />
+    {isSuperAdmin && <AdminActivityLog />}
     <AdminUserDirectoryPanel />
   </div>
 }
