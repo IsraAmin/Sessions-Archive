53669eb9c581f2528dbc73e11bf1f439141c5617
Isra Amin <150552777+IsraAmin@users.noreply.github.com>
2026-08-29 16:26:59 +0300

    Record every platform visit

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import { recordPlatformVisit } from './lib/analytics'
 import './styles.css'
 import './profile-pwa.css'
 import './app-v2.css'
@@ -33,3 +34,5 @@ if ('serviceWorker' in navigator) {
 createRoot(document.getElementById('root')!).render(
   <StrictMode><App /></StrictMode>
 )
+
+void recordPlatformVisit()
