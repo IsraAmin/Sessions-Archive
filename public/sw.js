396f3a81002416cea1c48f4cf50306cc54bcf14b
Isra Amin <150552777+IsraAmin@users.noreply.github.com>
2026-08-30 20:08:33 +0300

    Refresh app cache for cover focus controls

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const APP_CACHE = 'archive-repeat-app-v21'
-const RUNTIME_CACHE = 'archive-repeat-runtime-v21'
+const APP_CACHE = 'archive-repeat-app-v22'
+const RUNTIME_CACHE = 'archive-repeat-runtime-v22'
 const BASE_URL = new URL(self.registration.scope)
 const BASE_PATH = BASE_URL.pathname.endsWith('/') ? BASE_URL.pathname : `${BASE_URL.pathname}/`
 const APP_SHELL = [
