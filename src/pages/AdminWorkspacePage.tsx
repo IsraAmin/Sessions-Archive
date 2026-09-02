c0d88c3b16c79309bc9c9ae040da9d02381a141a
Isra Amin <150552777+IsraAmin@users.noreply.github.com>
2026-09-02 13:03:35 +0300

    Expose backup tools to super admin

--- src/pages/AdminWorkspacePage.tsx
+++ src/pages/AdminWorkspacePage.tsx
@@ -1,6 +1,7 @@
 import { AdminPage } from './AdminPage'
 import { AdminUserDirectoryPanel } from '../components/AdminUserDirectoryPanel'
 import { AdminActivityLog } from '../components/AdminActivityLog'
+import { AdminBackupRestorePanel } from '../components/AdminBackupRestorePanel'
 import { useAuth } from '../hooks/useAuth'
 
 export function AdminWorkspacePage() {
@@ -8,6 +9,7 @@ export function AdminWorkspacePage() {
 
   return <div className="admin-workspace-stack">
     <AdminPage />
+    {isSuperAdmin && <AdminBackupRestorePanel />}
     {isSuperAdmin && <AdminActivityLog />}
     <AdminUserDirectoryPanel />
   </div>
