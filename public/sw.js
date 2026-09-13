572d580b7ff61afd2210dd4526bea7dc01e8d4b2
Isra Amin <150552777+IsraAmin@users.noreply.github.com>
2026-09-13 23:22:39 +0300

    Refresh PWA cache for archive competition updates

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const APP_CACHE = 'archive-repeat-app-v29'
-const RUNTIME_CACHE = 'archive-repeat-runtime-v29'
+const APP_CACHE = 'archive-repeat-app-v30'
+const RUNTIME_CACHE = 'archive-repeat-runtime-v30'
 const BASE_URL = new URL(self.registration.scope)
 const BASE_PATH = BASE_URL.pathname.endsWith('/') ? BASE_URL.pathname : `${BASE_URL.pathname}/`
 const APP_SHELL = [
