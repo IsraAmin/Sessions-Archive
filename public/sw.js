528d9f4cec725521c47e95655cf9435679377a1e
Isra Amin <150552777+IsraAmin@users.noreply.github.com>
2026-09-13 21:43:58 +0300

    Bump PWA cache to refresh reverted public session routes

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const APP_CACHE = 'archive-repeat-app-v26'
-const RUNTIME_CACHE = 'archive-repeat-runtime-v26'
+const APP_CACHE = 'archive-repeat-app-v27'
+const RUNTIME_CACHE = 'archive-repeat-runtime-v27'
 const BASE_URL = new URL(self.registration.scope)
 const BASE_PATH = BASE_URL.pathname.endsWith('/') ? BASE_URL.pathname : `${BASE_URL.pathname}/`
 const APP_SHELL = [
